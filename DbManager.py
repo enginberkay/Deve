7cf959a222af8025da46b0e3e48010872f476191
enginberkay <berkayengin0@gmail.com>
2019-02-08 14:23:56 +0300

    files for queries are divided

--- DbManager.py
+++ DbManager.py
@@ -2,6 +2,7 @@ from subprocess import Popen, PIPE
 import Config
 import ExceptionManager
 from pathlib import Path
+from pkgutil import get_data
 
 exceptionFileName = "SQLPlus.py"
 
@@ -65,7 +66,7 @@ class Oracle:
 
     def getInvalidObjects(self, path):
         spoolPath = "spool " + str(path.resolve() / "InvalidObjects.log") + "; \n"
-        
+        query = get_data('sql', 'invalid_object_list.sql').decode('UTF-8')
         session = Popen(['sqlplus', '-S', self.__connectString],
                         stdin=PIPE, stdout=PIPE, stderr=PIPE)
         session.stdin.write(b"SET WRAP OFF; \n")
@@ -73,16 +74,16 @@ class Oracle:
         session.stdin.write(b"SET LINESIZE 32000; \n")
         session.stdin.write(b"set trimspool on; \n")
         session.stdin.write(bytes(spoolPath,"utf-8"))
-        session.stdin.write(b"SELECT OWNER, NAME, TYPE, LINE, TEXT FROM ALL_ERRORS;")
+        session.stdin.write(bytes(query, 'UTF-8'))
         session.stdin.write(b"\n spool off; \n")
         session.stdin.write(b"exit;")
         queryResult, errorMessage = session.communicate()
 
     def recompileInvalidObjects(self):
         sqlPath = Path("./recompile_invalid_objects.sql")
-        
+        query = get_data('sql', 'compile_invalid.sql').decode('UTF-8')
         session = Popen(['sqlplus', '-S', self.__connectString],
                         stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        session.stdin.write(b"exec dbms_utility.compile_schema(schema => null, compile_all => false);")
+        session.stdin.write(bytes(query, 'UTF-8'))
         session.stdin.write(b"exit;")
         queryResult, errorMessage = session.communicate()
